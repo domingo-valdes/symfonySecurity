8f06270b582d5ca4e4e5bded6a94a9d96abd523c
Domingo Valdés <dvaldes3@uc.cl>
2021-05-06 10:14:49 -0400

    part 3 ready

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends BaseFixture
+{
+    protected function loadData(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $this->createMany(10, 'main-users', function($i){
+            $user = new User();
+            $user->setEmail(sprintf('spacebar%d@example.com', $i));
+            $user->setFirstName($this->faker->firstName);
+
+            return $user;
+        });
+
+        $manager->flush();
+    }
+}
