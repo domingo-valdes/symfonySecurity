4cc2bb7704f9dad7c465df92da3d0d10e4e92b02
Domingo Valdés <dvaldes3@uc.cl>
2021-05-06 13:31:37 -0400

    part 9 ready

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,4 +25,12 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+
+    }
 }
