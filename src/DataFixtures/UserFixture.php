2df5931fd86a1e2ed3dcde70ea1c1758cd0f63d3
Domingo Valdés <dvaldes3@uc.cl>
2021-05-06 17:13:17 -0400

    part 12 ready

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,9 +4,16 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    private $passwordEncode;
+
+    public function __construct( UserPasswordEncoderInterface $passwordEncoder )
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
     protected function loadData(ObjectManager $manager)
     {
         // $product = new Product();
@@ -16,6 +23,10 @@ class UserFixture extends BaseFixture
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'engage'
+            ));
 
             return $user;
         });
