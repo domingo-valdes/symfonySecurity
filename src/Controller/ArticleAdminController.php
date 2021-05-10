c1423c35f032b54a9870e4fc4fb062ee30527beb
Domingo Valdés <dvaldes3@uc.cl>
2021-05-10 17:29:47 -0400

    part 33 ready

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -28,12 +28,10 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE",subject="article")
      */
     public function edit(Article $article)
     {
-        if ($article->getAuthor() != $this->getUser() &&  !$this->isGranted('ROLE_ADMIN_ARTICLE')){
-            throw $this->createAccessDeniedException('No access!');
-        }
         dd($article);
     }
 }
